5440e0a6832379b0e6d93055d41095a078162b3c
Theo-flux <ekunnusitheophilus@gmail.com>
2023-07-13 20:19:49 +0000

    in and out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,16 @@
+-- create table called `users`
+-- requirements:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN,
+-- never null default will be the first element
+-- of the enumeration, here US
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+	PRIMARY KEY (id)
+);
+
