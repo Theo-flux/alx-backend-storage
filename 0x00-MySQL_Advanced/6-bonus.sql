0fb41237254dc93791652d712296917704c94422
Theo-flux <ekunnusitheophilus@gmail.com>
2023-07-15 21:46:00 +0000

    ComputeAverageScoreForUser procedure on corrections table

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,5 +1,6 @@
 -- script that creates a stored procedure AddBonus
 -- that adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 CREATE PROCEDURE AddBonus(
 	IN user_id INTEGER,
