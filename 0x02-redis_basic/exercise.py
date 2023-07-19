13fa210b9cce8cdb9b10a7d44e2bb69004e3615b
Theo <ekunnusitheophilus@gmail.com>
2023-07-19 17:36:37 +0100

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """
 exercise file
 """
-from typing import Union, Optional, Callable
+from typing import Union, Optional, Callable, Awaitable, Any
 import redis
 import uuid
 from functools import wraps
@@ -11,16 +11,33 @@ from functools import wraps
 def count_calls(method: Callable) -> Callable:
 
     @wraps(method)
-    def counter(self, *args, **kwds):
+    def counter(self, *args):
         qual_name = method.__qualname__
 
         if self._redis.get(qual_name):
             self._redis.incr(qual_name, 1)
         else:
             self._redis.set(qual_name, 1)
-    
-        return method(self, *args, **kwds)
+
+        return method(self, *args)
+
     return counter
+
+def call_history(method: Callable) -> Callable:
+
+    @wraps(method)
+    def call(self, *args):
+        qual_name = method.__qualname__
+
+        for arg in args:
+            self._redis.rpush(f"{qual_name}:inputs",  arg)
+    
+        res = method(self, *args)
+        self._redis.rpush(f"{qual_name}:outputs", res)
+
+        return res
+
+    return call
 
 
 class Cache():
@@ -36,22 +53,23 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None):
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[Awaitable, Any]:
         val = self._redis.get(key)
         if fn:
             return fn(val)
         return val
 
-    def get_str(self, key: str):
+    def get_str(self, key: str) -> Union[Awaitable, str]:
         return self.get(key, fn=lambda d: d.decode("utf-8") if d else None)
 
-    def get_int(self, key: str):
+    def get_int(self, key: str) -> Union[Awaitable, int]:
         return self.get(key, fn=lambda d: int(d) if d else None)
         
 
