83e7cb3a9964c4d903972c76cae4b20901d3d7bb
Theo <ekunnusitheophilus@gmail.com>
2023-07-20 03:18:06 +0100

    fix: Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,40 +12,24 @@ db = redis.Redis()
 
 def count_url(method: Callable):
     @wraps(method)
-    def counter(*args):
-        count_key = f"count:{args}"
-        method(*args)
+    def counter(arg):
+        cached_url = f"cached:{arg}"
+        data = db.get(cached_url)
 
-        if (db.get(count_key)):
-            db.incr(count_key)
-        else:
-            db.setex(count_key, 10, 1)
+        if data:
+            return data.decode("utf-8")
 
-        # print(db.get(count_key))
-        return db.get(count_key)
+        count_key = f"count:{url}"
+        res = method(url)
 
-    return counter
-
-
-def cache_url(method: Callable):
-    @wraps(method)
-    def counter(*args):
-        cache_key = f"cache:{args}"
-        cache = method(*args)
-
-        if (db.get(cache_key)):
-            db.set(cache_key, cache)
-        else:
-            db.set(cache_key, cache)
-
-        # print(cache)
-        return cache
+        db.incr(count_key)
+        db.setex(cached_url, 10, html)
+        return res
 
     return counter
 
 
 @count_url
-@cache_url
 def get_page(url: str) -> str:
     """_summary_
 
